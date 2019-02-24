b167fa89cca16a301262823525305a030181aac4
Aida Manna <aidamanna@gmail.com>
2019-02-24 18:31:09 +0100

    Initial commit

---
+++ src/main/java/org/example/devicenator/DevicenatorApplication.java
@@ -0,0 +1,13 @@
+package org.example.devicenator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicenatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicenatorApplication.class, args);
+    }
+
+}
