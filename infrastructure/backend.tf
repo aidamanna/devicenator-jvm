5bb5619ed3d9d04320922313d2333430e92ac4d4
Aida Manna <aidamanna@gmail.com>
2020-04-10 20:20:41 +0200

    Move terraform backend configuration to main.tf

--- infrastructure/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket = "devicenator-terraform-state-bucket"
-    region = "eu-west-1"
-    key = "devicenator-api-terraform.tfstate"
-    dynamodb_table = "devicentator-terraform-state-lock"
-  }
-}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "devicenator-terraform-state-bucket"
+    region = "eu-west-1"
+    key = "devicenator-api-terraform.tfstate"
+    dynamodb_table = "devicentator-terraform-state-lock"
+  }
+}
+
 provider "aws" {
   profile = "default"
   region = var.region
