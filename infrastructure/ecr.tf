cf86cf0d440eb4a1423780abeaf4ba29155cf343
Aida Manna <aidamanna@gmail.com>
2020-04-18 14:13:23 +0200

    Create ecr repository and role to access it

---
+++ infrastructure/ecr.tf
@@ -0,0 +1,52 @@
+resource "aws_ecr_repository" "devicenator-api-repository" {
+  name = "devicenator-api-repository"
+}
+
+resource "aws_iam_user" "travis-devicenator-api" {
+  name = "travis-devicenator-api"
+}
+
+//resource "aws_iam_access_key" "travis-devicenator-api" {
+//  user = aws_iam_user.travis-devicenator-api.name
+//}
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+  assume_role_policy = data.aws_iam_policy_document.assume-rol-policy-document.json
+}
+
+data "aws_iam_policy_document" "assume-rol-policy-document" {
+  statement {
+    actions = [
+      "sts:AssumeRole"]
+
+    principals {
+      identifiers = [
+        "arn:aws:iam::${var.account_id}:user/travis-devicenator-api"]
+      type = "AWS"
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecr-policy-attachment" {
+  role       = aws_iam_role.ecr-role.name
+  policy_arn = aws_iam_policy.ecr-policy.arn
+}
+
+resource "aws_iam_policy" "ecr-policy" {
+  name        = "ecr-policy"
+  description = "Policy for accessing ECR"
+  policy = data.aws_iam_policy_document.ecr-policy-document.json
+}
+
+data "aws_iam_policy_document" "ecr-policy-document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:*"
+    ]
+    resources = [
+      "arn:aws:ecr:${var.region}:${var.account_id}:repository/devicenator-api-repository"
+    ]
+  }
+}
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,5 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+variable "account_id" {}
