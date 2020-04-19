2ab4fead298fc9bc82f29100eee8c61590d3e216
Aida Manna <aidamanna@gmail.com>
2020-04-19 10:44:21 +0200

    Refactor build-docker-image script

--- infrastructure/build-docker-image.sh
+++ infrastructure/build-docker-image.sh
@@ -3,8 +3,7 @@
 set -euxo pipefail
 
 REPOSITORY="832237568284.dkr.ecr.eu-west-1.amazonaws.com"
-REPOSITORY_NAME="devicenator-api"
-AWS_REGION="eu-west-1"
+IMAGE_TAG="${REPOSITORY}/devicenator-api:devicenator-api-${SHA}"
 ROLE_ARN="arn:aws:iam::832237568284:role/ecr-role"
 
 ASSUMED_ROLE=$(
@@ -18,10 +17,9 @@ export AWS_ACCESS_KEY_ID=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.AccessKeyI
 export AWS_SECRET_ACCESS_KEY=$(echo ${ASSUMED_ROLE} | jq -r '.Credentials.SecretAccessKey')
 
 aws ecr get-login-password \
-    --region ${AWS_REGION} \
 | docker login \
     --username AWS \
     --password-stdin ${REPOSITORY}
 
-docker build -t ${REPOSITORY}/${REPOSITORY_NAME}:${SHA} .
-docker push ${REPOSITORY}/${REPOSITORY_NAME}:${SHA}
+docker build -t ${IMAGE_TAG} .
+docker push ${IMAGE_TAG}
