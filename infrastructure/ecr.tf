e218e63e9280e63d38d850c0b5064ca8b152823c
Aida Manna <aidamanna@gmail.com>
2020-04-19 11:53:15 +0200

    Add lifecycle policy to ecr repository

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -2,6 +2,29 @@ resource "aws_ecr_repository" "devicenator-api" {
   name = local.repository_name
 }
 
+resource "aws_ecr_lifecycle_policy" "ecr-lifecycle-policy" {
+  repository = aws_ecr_repository.devicenator-api.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 10 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 10
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_user" "travis-devicenator-api" {
   name = local.travis_user
 }
