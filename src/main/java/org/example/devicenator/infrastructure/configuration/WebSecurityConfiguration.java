ef8405fbd3ceca610371943ffc65ee360e548e18
Aida Manna <aidamanna@gmail.com>
2020-04-25 19:59:30 +0200

    Remove auth from /health endpoint

--- src/main/java/org/example/devicenator/infrastructure/configuration/WebSecurityConfiguration.java
+++ src/main/java/org/example/devicenator/infrastructure/configuration/WebSecurityConfiguration.java
@@ -52,7 +52,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests().antMatchers("/authenticate", "/register").permitAll()
+            .authorizeRequests().antMatchers("/authenticate", "/register", "/actuator/health").permitAll()
             .anyRequest().authenticated().and()
             .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
             .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
